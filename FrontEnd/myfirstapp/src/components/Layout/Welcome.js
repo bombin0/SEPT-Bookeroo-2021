e61d6a1752d017897b8465fa853edf175062939f
[Vincent Tang s3645447] <s3645447@student.rmit.edu.au>
2021-08-22 23:34:11 +1000

    homepage prototype

--- FrontEnd/myfirstapp/src/components/Layout/Welcome.js
+++ FrontEnd/myfirstapp/src/components/Layout/Welcome.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import monkeyImage from "../../images/monkey.jpg";
+import bookerooHome from "../../images/BookerooHome.jpg";
+import email from "../../images/email.png";
+import phone from "../../images/phone.png";
+import support from "../../images/support.png";
 
 class Welcome extends Component {
   render() {
@@ -9,21 +12,20 @@ class Welcome extends Component {
         <div className="flex-container">
 
           <div className="child">
-            <img src={monkeyImage} />
+            <img src={bookerooHome}/>
 
 
           </div>
           <div className="child">
-            <h1 id="welcome">Welcome to Bookeroo</h1>
-          <div classname= "subtext">
-            <h2>The number one website to buy or sell books</h2>
-          </div>
+            <h1 id="welcome">Welcome to <br/>Bookeroo</h1>
+            <h1 id="subtext">The number one <br/>website to buy and sell <br/> books</h1>
+           
             <div>
               <span className="button">
-                Login
+                <a href = "#"> Login </a>
               </span>
               <span className="button">
-                Register
+                <a href = "#"> Register </a>
               </span>
             </div>
           </div>
@@ -32,31 +34,50 @@ class Welcome extends Component {
         <div className="flex-container">
 
           <div className="child">
-            <img src={monkeyImage} />
+            <img src={bookerooHome} />
 
           </div>
 
           <div className="child">
-            <h2>Browse book library</h2>
-            <h4>Browse our wonderful selection of books here</h4>
+            <h2 id="browse">Browse Book Library</h2>
+            <body id="browse-text">Browse BOOKEROO's wide selection of books ranging from latest release books, bestselling authors, timeless classics, and many more!  </body>
 
-            <a href="#"><u>Read more</u></a>
+            <a id ="read-more-link" href="#"><u>Learn more → </u></a>
             <div>
 
             </div>
           </div>
         </div>
 
-        <div className = "flex-container">
-        <h1> Contact Us</h1>
-        
-        <br/><h4> Lorem </h4>
+        <div className="flex-container">
+          <div className="contact-us-child">
+            <h1> Contact Us</h1>
+            <body id = "enquiries"> If you have any enquiries, feel free to contact the customer service team using any of the contact methods below! </body>
+          </div>
 
+
+          </div>
+
+          <div className="flex-container">
+          <div className="info-child">
+            <img className = "img" src={email}/>
+            <h1> Email</h1>
+            <body> BOOKEROO@BOOKEROO.COM </body>
+          </div>
+          <div className="info-child">
+          <img className = "img" src={phone}/>
+            <h1> Phone</h1>
+            <body> +61(XXX-XXX-XXX) </body>
+          </div>
+          <div className="info-child">
+          <img className = "img" src={support}/>
+            <h1> Support</h1>
+            <body> BOOKEROO's online hours of operations are:<br/> Monday to Friday 9.00am to 5.00pm <br/> (Please note all hours are Australian Eastern Standard Time) </body>
+          </div>
+
+
+          </div>
         </div>
-
-
-        
-      </div>
         );
   }
 }
